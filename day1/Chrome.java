484116bc96cf71bfee54b0fc582650587eb52756
MOZHIII <arthiarulmozhi97@gmail.com>
2026-06-28 08:41:15 +0530

    Add files via upload
    day1 assignments

---
+++ day1/Chrome.java
@@ -0,0 +1,17 @@
+package org.testleaf.week1.day1;
+
+public class Chrome {
+    public static void main(String[] args) {
+    float version = 91.0f;
+    String developer = "Google";
+    boolean isBeta = false;
+    int releaseYear = 2008;
+    char shortcutKey = 'C';
+    System.out.println("Browser version is " +version+
+    "\ndeveloper name is "+developer+ "\nis beta is " +isBeta+
+    "\nwith year "+releaseYear+ "\nand key is " +shortcutKey
+    );
+    
+}
+
+}
